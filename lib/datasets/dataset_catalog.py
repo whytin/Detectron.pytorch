c93c3b75179f59761cee3207b7a65d278603ef7f
adityaarun1 <adityaarun1@gmail.com>
2018-05-23 16:41:25 +0530

    add/fix voc dataset options

--- lib/datasets/dataset_catalog.py
+++ lib/datasets/dataset_catalog.py
@@ -193,6 +193,14 @@ DATASETS = {
         ANN_FN:
             _DATA_DIR + '/coco/annotations/image_info_test2017.json'
     },
+    'voc_2007_train': {
+        IM_DIR:
+            _DATA_DIR + '/VOC2007/JPEGImages',
+        ANN_FN:
+            _DATA_DIR + '/VOC2007/annotations/voc_2007_train.json',
+        DEVKIT_DIR:
+            _DATA_DIR + '/VOC2007/VOCdevkit2007'
+    },
     'voc_2007_trainval': {
         IM_DIR:
             _DATA_DIR + '/VOC2007/JPEGImages',
