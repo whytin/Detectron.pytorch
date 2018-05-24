f366622d2e0fd69dddd1cc9b363d97032d64f68e
adityaarun1 <adityaarun1@gmail.com>
2018-05-24 11:47:07 +0530

    add voc_xml2json++ file

--- tools/pascal_voc_xml2json.py
+++ tools/pascal_voc_xml2json.py
@@ -2,18 +2,6 @@ import xml.etree.ElementTree as ET
 import os
 import json
 
-coco = dict()
-coco['images'] = []
-coco['type'] = 'instances'
-coco['annotations'] = []
-coco['categories'] = []
-
-category_set = dict()
-image_set = set()
-
-category_item_id = 0
-image_id = 20180000000
-annotation_id = 0
 
 def addCatItem(name):
     global category_item_id
@@ -25,6 +13,7 @@ def addCatItem(name):
     coco['categories'].append(category_item)
     category_set[name] = category_item_id
     return category_item_id
+
 
 def addImgItem(file_name, size):
     global image_id
@@ -44,22 +33,23 @@ def addImgItem(file_name, size):
     image_set.add(file_name)
     return image_id
 
+
 def addAnnoItem(object_name, image_id, category_id, bbox):
     global annotation_id
     annotation_item = dict()
     annotation_item['segmentation'] = []
     seg = []
-    #bbox[] is x,y,w,h
-    #left_top
+    # bbox[] is x,y,w,h
+    # left_top
     seg.append(bbox[0])
     seg.append(bbox[1])
-    #left_bottom
+    # left_bottom
     seg.append(bbox[0])
     seg.append(bbox[1] + bbox[3])
-    #right_bottom
+    # right_bottom
     seg.append(bbox[0] + bbox[2])
     seg.append(bbox[1] + bbox[3])
-    #right_top
+    # right_top
     seg.append(bbox[0] + bbox[2])
     seg.append(bbox[1])
 
@@ -73,13 +63,14 @@ def addAnnoItem(object_name, image_id, category_id, bbox):
     annotation_item['category_id'] = category_id
     annotation_id += 1
     annotation_item['id'] = annotation_id
-    coco['annotations'].append(annotation_item)
+    return annotation_item
 
-def parseXmlFiles(xml_path): 
+
+def parseXmlFiles(xml_path, file_list):
     for f in os.listdir(xml_path):
-        if not f.endswith('.xml'):
+        if (not f.endswith('.xml')) and (not any(f in fl for fl in file_list)):
             continue
-        
+
         bndbox = dict()
         size = dict()
         current_image_id = None
@@ -95,36 +86,38 @@ def parseXmlFiles(xml_path):
         tree = ET.parse(xml_file)
         root = tree.getroot()
         if root.tag != 'annotation':
-            raise Exception('pascal voc xml root element should be annotation, rather than {}'.format(root.tag))
+            raise Exception(
+                'pascal voc xml root element should be annotation, rather than {}'.
+                format(root.tag))
 
-        #elem is <folder>, <filename>, <size>, <object>
+        # elem is <folder>, <filename>, <size>, <object>
         for elem in root:
             current_parent = elem.tag
             current_sub = None
             object_name = None
-            
+
             if elem.tag == 'folder':
                 continue
-            
+
             if elem.tag == 'filename':
                 file_name = elem.text
                 if file_name in category_set:
                     raise Exception('file_name duplicated')
-                
-            #add img item only after parse <size> tag
+
+            # add img item only after parse <size> tag
             elif current_image_id is None and file_name is not None and size['width'] is not None:
                 if file_name not in image_set:
                     current_image_id = addImgItem(file_name, size)
                     print('add image with {} and {}'.format(file_name, size))
                 else:
-                    raise Exception('duplicated image: {}'.format(file_name)) 
-            #subelem is <width>, <height>, <depth>, <name>, <bndbox>
+                    raise Exception('duplicated image: {}'.format(file_name))
+            # subelem is <width>, <height>, <depth>, <name>, <bndbox>
             for subelem in elem:
-                bndbox ['xmin'] = None
-                bndbox ['xmax'] = None
-                bndbox ['ymin'] = None
-                bndbox ['ymax'] = None
-                
+                bndbox['xmin'] = None
+                bndbox['xmax'] = None
+                bndbox['ymin'] = None
+                bndbox['ymax'] = None
+
                 current_sub = subelem.tag
                 if current_parent == 'object' and subelem.tag == 'name':
                     object_name = subelem.text
@@ -138,14 +131,15 @@ def parseXmlFiles(xml_path):
                         raise Exception('xml structure broken at size tag.')
                     size[subelem.tag] = int(subelem.text)
 
-                #option is <xmin>, <ymin>, <xmax>, <ymax>, when subelem is <bndbox>
+                # option is <xmin>, <ymin>, <xmax>, <ymax>, when subelem is <bndbox>
                 for option in subelem:
                     if current_sub == 'bndbox':
                         if bndbox[option.tag] is not None:
-                            raise Exception('xml structure corrupted at bndbox tag.')
+                            raise Exception(
+                                'xml structure corrupted at bndbox tag.')
                         bndbox[option.tag] = int(option.text)
 
-                #only after parse the <object> tag
+                # only after parse the <object> tag
                 if bndbox['xmin'] is not None:
                     if object_name is None:
                         raise Exception('xml structure broken at bndbox tag')
@@ -154,19 +148,54 @@ def parseXmlFiles(xml_path):
                     if current_category_id is None:
                         raise Exception('xml structure broken at bndbox tag')
                     bbox = []
-                    #x
+                    # x
                     bbox.append(bndbox['xmin'])
-                    #y
+                    # y
                     bbox.append(bndbox['ymin'])
-                    #w
+                    # w
                     bbox.append(bndbox['xmax'] - bndbox['xmin'])
-                    #h
+                    # h
                     bbox.append(bndbox['ymax'] - bndbox['ymin'])
-                    print('add annotation with {},{},{},{}'.format(object_name, current_image_id, current_category_id, bbox))
-                    addAnnoItem(object_name, current_image_id, current_category_id, bbox )
+                    print('add annotation with {},{},{},{}'.format(
+                        object_name, current_image_id, current_category_id,
+                        bbox))
+                    return addAnnoItem(object_name, current_image_id,
+                                       current_category_id, bbox)
+
 
 if __name__ == '__main__':
+    image_sets_path = 'ImageSets/Main'
+    anno_path = 'annotations'
     xml_path = 'Annotations'
-    json_file = 'instances.json'
-    parseXmlFiles(xml_path)
-    json.dump(coco, open(json_file, 'w'))
+    for f in os.listdir(image_sets_path):
+        if not f.endswith('.txt'):
+            continue
+
+        coco = dict()
+        coco['images'] = []
+        coco['type'] = 'instances'
+        coco['annotations'] = []
+        coco['categories'] = []
+
+        category_set = dict()
+        image_set = set()
+
+        category_item_id = 0
+        image_id = 20180000000
+        annotation_id = 0
+
+        image_set_file = os.path.join(image_sets_path, f)
+
+        file_list = []
+        with open(image_set_file, 'r') as isf:
+            lines = isf.readlines()
+            for line in lines:
+                file_list.append(line.strip() + '.xml')
+
+        annotation_item = parseXmlFiles(xml_path, file_list)
+        coco['annotations'].append(annotation_item)
+
+        json_file = f.split('.')[0] + '.json'
+        json_file_path = os.path.join(anno_path, json_file)
+        print('Writing to file {0}'.format(json_file_path))
+        json.dump(coco, open(json_file_path, 'w'))
